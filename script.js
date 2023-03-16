32341d08fb92201b1146f1344fe02aa8257a00d9
juandario92 <juandariorojas1992@gmail.com>
2023-03-16 13:26:16 -0300

    Fetching Data

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+// Unsplash API
+
+const count = 10;
+const apiKey = "7IPvGoAMuhYIc1A0jCO3Ix01k1dVJ02gP6eD_XN0xy0";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+
+// Get photos from unsplash API
+
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data); 
+
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// On Load 
+getPhotos();
